dc71ced9fefbff16b85cecb1bea5e51454b601f0
swaroop <S525756@mail.nwmissouri.edu>
2018-02-12 23:52:25 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,22 @@ public class Main {
       OptionalDouble c = p.stream().mapToInt(i -> i*8).average();
       System.out.println("streams"+c);
         System.out.println("Hello World!");
+        
+        List<String> alphaUpper = new ArrayList<>();
+        for (String s : alpha) {
+            alphaUpper.add(s.toUpperCase());
+        }
+
+        System.out.println(alpha); //[a, b, c, d]
+        System.out.println(alphaUpper); //[A, B, C, D]
+
+        // Java 8
+        List<String> collect = alpha.stream().map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(collect); //[A, B, C, D]
+
+        // Extra, streams apply to any data type.
+        List<Integer> num = Arrays.asList(1,2,3,4,5);
+        List<Integer> collect1 = num.stream().map(n -> n * 2).collect(Collectors.toList());
+        System.out.println(collect1); //[2, 4, 6, 8, 10]
     }
 }
